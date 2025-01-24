828843aaa4c750a4c61fd130d974e266f5b6c979
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2025-01-24 20:22:19 +0600

    Add models and serializers for auth

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("ADMIN", "Admin"),
+        ("MEMBER", "Member"),
+    )
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="MEMBER")
+
+    def is_admin(self):
+        return self.role == "ADMIN"
